b39b7b250a2a570ab83ac68a01a2070f16017052
david <daviddecastro42@live.co.uk>
2021-07-03 23:08:05 +0100

    finished the feature component and use the data

--- src/components/feature-card.js
+++ src/components/feature-card.js
@@ -5,11 +5,16 @@ export default function FeatureCard({
   src,
   altText = 'default alt text',
   title,
-  text,
+  text
 }) {
   return (
-    <h1>FeatureCard</h1>
-
+    <Box sx={styles.card}>
+      <Image src={src} altText={altText} sx={styles.img} />
+      <Box sx={styles.wrapper}>
+        <Heading sx={styles.wrapper.title}>{title}</Heading>
+        <Text sx={styles.wrapper.subTitle}>{text} </Text>
+      </Box>
+    </Box>
   );
 }
 
@@ -17,7 +22,7 @@ const styles = {
   card: {
     display: 'flex',
     alignItems: 'flex-start',
-    mb: -1,
+    mb: -1
   },
 
   img: {
@@ -25,7 +30,7 @@ const styles = {
     height: 'auto',
     flexShrink: 0,
     mr: [2, 3, null, null, 4, 5],
-    ml: -2,
+    ml: -2
   },
   wrapper: {
     width: '100%',
@@ -37,13 +42,13 @@ const styles = {
       color: 'heading_secondary',
       lineHeight: 1.4,
       fontWeight: 700,
-      mb: ['10px', null, '15px'],
+      mb: ['10px', null, '15px']
     },
 
     subTitle: {
       fontSize: [1, '15px'],
       fontWeight: 400,
-      lineHeight: '1.9',
-    },
-  },
+      lineHeight: '1.9'
+    }
+  }
 };
